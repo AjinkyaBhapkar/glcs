abe324ab33c355c609cb3fcbce8f3272c5fba8db
Ajinkya Bhapkar <ajinkyabhapkar138@gmail.com>
2024-08-24 22:50:15 +0530

    working

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.ATLAS_URI; 
 mongoose.connect(uri, { useNewUrlParser: true });
-
+ 
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connected!!!");
